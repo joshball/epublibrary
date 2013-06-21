cec5d007cb9756869d259ffbbf163fc0fe45294c
lordkiron@gmail.com <lordkiron@gmail.com@46963b11-3e41-8eae-6632-a22001bf90fb>
2013-06-21 20:06:22 +0000

    Added code to process and add Apple ePub v2 extensions

---
+++ trunk/AppleEPubV2Extensions/AppleTargetPlatform.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPubLibrary.AppleEPubV2Extensions
+{
+    public enum PlatformType
+    {
+        All,
+        iPad,
+        iPhone,
+    }
+
+
+    public enum OrientationLock
+    {
+        Off,
+        LandscapeOnly,
+        PortraitOnly,
+    }
+
+    public class AppleTargetPlatform
+    {
+        private PlatformType _type = PlatformType.All;
+        private OrientationLock _lockType = OrientationLock.Off;
+        private bool _allowCustomFonts = true;
+        private bool _openToSpread = false;
+        private bool _fixedLayout = false;
+
+
+        public PlatformType Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public OrientationLock OrientationLockType
+        {
+            get { return _lockType; }
+            set { _lockType = value; }
+        }
+
+        public bool CustomFontsAllowed
+        {
+            get { return _allowCustomFonts; }
+            set { _allowCustomFonts = value; }
+        }
+
+        public bool OpenToSpread
+        {
+            get { return _openToSpread; }
+            set { _openToSpread = value; }
+        }
+
+        public bool FixedLayout
+        {
+            get { return _fixedLayout; }
+            set { _fixedLayout = value; }
+        }
+
+        public static string ConvertTypeToString(PlatformType type)
+        {
+            if (type == PlatformType.iPad)
+            {
+                return "ipad";
+            }
+            if (type == PlatformType.iPhone)
+            {
+                return "iphone";
+            }
+            return "*";
+        }
+
+
+    }
+}
