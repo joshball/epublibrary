9df99e0d95ab79f6268b5b805a4a69efad938afa
lordkiron@gmail.com <lordkiron@gmail.com@46963b11-3e41-8eae-6632-a22001bf90fb>
2014-12-21 21:02:37 +0000

    Made manifest item generate it's own code

--- Content/Manifest/ManifestItemV3.cs
+++ Content/Manifest/ManifestItemV3.cs
@@ -1,4 +1,8 @@
-﻿namespace EPubLibrary.Content.Manifest
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace EPubLibrary.Content.Manifest
 {
     class ManifestItemV3
     {
@@ -63,5 +67,87 @@
         /// The switch property indicates that the described Publication Resource contains one or more instances of the epub:switch element.
         /// </summary>
         public bool Switch { get; set; }
+
+        public XElement GenerateElement()
+        {
+            var tocElement = new XElement(EPubNamespaces.OpfNameSpace + "item");
+            tocElement.Add(new XAttribute("id", ID));
+            tocElement.Add(new XAttribute("href", HRef));
+            tocElement.Add(new XAttribute("media-type", MediaType.GetAsSerializableString()));
+            if (!string.IsNullOrEmpty(Fallback))
+            {
+                tocElement.Add(new XAttribute("fallback", Fallback));
+            }
+
+            List<string> properties;
+            BuildPropertiesList(out properties);
+
+
+            if (properties.Count > 0)
+            {
+                var sb = new StringBuilder();
+                bool first = true;
+                foreach (var property in properties)
+                {
+                    if (!first)
+                    {
+                        sb.AppendFormat(" {0}", property);
+                    }
+                    else
+                    {
+                        sb.AppendFormat("{0}", property);
+                        first = false;
+                    }
+                }
+                tocElement.Add(new XAttribute("properties", sb.ToString()));
+            }
+
+            if (!string.IsNullOrEmpty(MediaOverlay))
+            {
+                tocElement.Add(new XAttribute("media-overlay", MediaOverlay));
+            }
+            return tocElement;
+        }
+
+        private void BuildPropertiesList(out List<string> properties)
+        {
+            properties = new List<string>();
+
+            if (ContainsRemoteResources)
+            {
+                properties.Add("remote-resources");
+            }
+
+            if (CoverImage)
+            {
+                properties.Add("cover-image");
+            }
+
+            if (MathML)
+            {
+                properties.Add("mathml");
+            }
+
+            if (Nav)
+            {
+                properties.Add("nav");
+            }
+
+            if (SVG)
+            {
+                properties.Add("svg");
+            }
+
+            if (Scripted)
+            {
+                properties.Add("scripted");
+            }
+
+            if (Switch)
+            {
+                properties.Add("switch");
+            }
+        }
+
     }
 }
